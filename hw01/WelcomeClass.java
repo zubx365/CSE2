3c92d2c75ef7c23ffec0dc73114fa0beabd6fbe8
zubx365 <mjc714@lehigh.edu>
2014-08-27 03:54:29 +0000

    created and updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,21 @@
+//Matthew Chin
+//Welcome Class Program
+
+//define class
+public class WelcomeClass{
+    //main method
+    public static void main(String[] args){
+        
+    // print welcome message
+        System.out.println("   ----------- ");
+        System.out.println("   | WELCOME | ");
+        System.out.println("   ----------- ");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-M--J--C--7--1--4-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("I like cats and Counter-Strike.");
+    }
+    
+}
