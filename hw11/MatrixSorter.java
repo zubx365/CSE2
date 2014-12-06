e22f2b32c985611e2f5e930cbff36d6e4d91f1ab
zubx365 <mjc714@lehigh.edu>
2014-12-06 03:28:39 +0000

    updated files

--- hw11/MatrixSorter.java
+++ hw11/MatrixSorter.java
@@ -24,44 +24,62 @@ public class MatrixSorter {
             //display the 3d array mat3d
             //    show(mat3d);
             //output message to tell user smallest entry in 3d matrix
-        //    System.out.println("The smallest entry in the 3D matrix is " +
-                    //        findMin(mat3d)); //method searches 3d matrix for lowest entry and returns value
+            //    System.out.println("The smallest entry in the 3D matrix is " +
+            //        findMin(mat3d)); //method searches 3d matrix for lowest entry and returns value
 
-                    //output message notifying user that the 3d matrix will be sorted, then returned by the call to show(). 
-                    System.out.println("After sorting the 3rd matrix we get");
-                    //sorts the 3d matrix per specifications above
-                    //    sort(mat3d[2]);
-                    //display the 3d matrix after it has been sorted
-                    //    show(mat3d);
+            //output message notifying user that the 3d matrix will be sorted, then returned by the call to show(). 
+            System.out.println("After sorting the 3rd matrix we get");
+            //sorts the 3d matrix per specifications above
+            //    sort(mat3d[2]);
+            //display the 3d matrix after it has been sorted
+            //    show(mat3d);
 
 
 
-                } //end main 
+        } //end main 
 
-            //this method creates a 3d ragged array containing random elements from [1-99]
-            public static int[][][] buildMat3d() {
+    //this method creates a 3d ragged array containing random elements from [1-99]
+    public static int[][][] buildMat3d() {
 
-                    int[][][] matrix1 = new int[3][][]; //initialzes and allocates memory for 3d matrix
+            int[][][] matrix1 = new int[3][][]; //initialzes and allocates memory for 3d matrix
 
-                    matrix1[0] = new int[3][3]; //sets slab 1 to have 3 rows and 3 columns
-                    matrix1[1] = new int[5][6]; //sets slab 2 to have 5 rows and 6 columns
-                    matrix1[2] = new int[7][9]; //sets slab 3 to have 7 rows and 9 columns
+            matrix1[0] = new int[3][3]; //sets slab 1 to have 3 rows and 3 columns
+            matrix1[1] = new int[5][6]; //sets slab 2 to have 5 rows and 6 columns
+            matrix1[2] = new int[7][9]; //sets slab 3 to have 7 rows and 9 columns
 
-                    int x = 0;//counter var for slab iteration
-                    //prints out slab 1
-                    for (int row = 0; row < 3; row++) { //loops through the rows in slab n
+            
+            //prints out slab 1
+            for (int i = 0; i < 2; i++) {
+                for (int row = 0; row < 7; row++) { //loops through the rows in slab n
 
-                        for (int column = 0; column < matrix1[row].length; column++) {    //loops through the columns in slab n, row n
+                    for (int column = 0; column < 9; column++) { //loops through the columns in slab n, row n
 
-                            //assigns the respective slab,row,column to a random value in range of [1-99]
-                            matrix1[x][row][column] = (int)((Math.random() * 99) + 1); 
-                            
-                            System.out.print(matrix1[x][row][column] + " "); //test
-                            x++;    //increments x to increment to next slab
-                        } //end inner for
-                        System.out.println();//test
-                    } //end outer for
-                return matrix1; //returns the populated matrix
-                } //end buildMat3d method
+                        //assigns the respective slab,row,column to a random value in range of [1-99]
+                        matrix1[i][row][column] = (int)((Math.random() * 99) + 1); 
 
-        } //end MatrixSorter class
+                        System.out.print(matrix1[i][row][column] + " "); //test
+                        
+                    } //end inner for
+                    System.out.println(); //test
+                } //end outer for
+
+            }//end slab for
+            
+            return matrix1; //returns the populated matrix
+        } //end buildMat3d method
+
+        //show method-displays the contents of the 3d matrix
+        public static int[][][] show(int[][][] matrix){
+            
+            for(int i = 0; i <3; i++){  //loops through slabs
+                for(int i = 0; i < 7; i++){    //loops through rows
+                    for(int i = 0; i< 9; i++){  //loops through columns
+                        
+                        System.out.print(matrix[][][] + " ");   //prints out each slab/row/column
+                    }//end 2nd inner loop
+                    System.out.println();   //makes new line 
+                }//end 1st inner for loop
+            }//end outer for loop
+        }//end show method
+        
+} //end MatrixSorter class
